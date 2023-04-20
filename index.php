cd1300ce4084cebbdd5d9660343082946c30700e
Tuvedunyel <mightygodx@gmail.com>
2023-04-20 11:51:34 +0200

    text align center

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script type="module" crossorigin src="/assets/index-045ad5a7.js"></script>
+    <link rel="stylesheet" href="/assets/index-58b50479.css">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <div id="root"></div>
+<?php wp_footer(); ?>
+</body>
+
+</html>
