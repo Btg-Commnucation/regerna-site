ea812daafd23a553641e312c76a3ba985355e595
Tuvedunyel <mightygodx@gmail.com>
2023-04-20 15:29:25 +0200

    favicon

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
-<!doctype html>
-<html <?php language_attributes(); ?>>
+<?php 
 
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script type="module" crossorigin src="/assets/index-045ad5a7.js"></script>
-    <link rel="stylesheet" href="/assets/index-58b50479.css">
-    <?php wp_head(); ?>
-</head>
+$url = "https://regerna.eu";
 
-<body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-    <div id="root"></div>
-<?php wp_footer(); ?>
-</body>
-
-</html>
+header("Location: $url");
+exit;
+?>
